b1c98df2f81d620546fb9463ccde42068705e2d3
Dmitry <parshin@phystech.edu>
2016-12-06 00:14:57 +0300

    final

--- src/com/netcracker/edu/parshin/autorent/Car.java
+++ src/com/netcracker/edu/parshin/autorent/Car.java
@@ -1,20 +1,24 @@
 package com.netcracker.edu.parshin.autorent;
 
-import com.sun.org.apache.xml.internal.serializer.OutputPropertiesFactory;
-import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -85,7 +89,7 @@ public class Car {
         this.price = price;
     }
     
-    public void listCar(){
+    public void listCar() {
         try {
             File xmlFile = new File("DataBase.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -122,9 +126,9 @@ public class Car {
                     }
                 }                    
             }
-        } catch (Exception e){
-            e.printStackTrace();
-        } 
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(Car.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void addCar(){
